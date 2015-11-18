a0f421fa00a4cabd3c71faba7bea9c945b3e6331
iceton <github@emailbad.com>
2015-11-18 11:25:29 -0800

    process info too

--- cpu_alert.rb
+++ cpu_alert.rb
@@ -1,11 +1,13 @@
 #!/usr/local/bin/ruby
 
-max = 3
+max_load = 3
 
 load = `sysctl -n vm.loadavg`
 puts load
-load5 = load.split(" ")[2].to_f
-puts load5
-if load5 > max
-  `osascript -e 'display notification "5 minute load is above max\n#{load5} > #{max}" with title "High Load"'`
+load_5min = load.split(" ")[2].to_f
+puts load_5min
+if load_5min > max_load
+  ps1 = `ps aux -r`.split("\n")[1].split(" ")
+  ps1_basename = File.basename(ps1.last)
+  `osascript -e 'display notification "#{load_5min} load > #{max_load} max\n#{ps1[2]}% #{ps1_basename}" with title "High Load"'`
 end
