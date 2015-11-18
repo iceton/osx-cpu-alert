2587bd44a9b0c984e7799f622934376c63903264
iceton <github@emailbad.com>
2015-11-18 11:02:11 -0800

    split instead of regex, load and max in message

--- cpu_alert.rb
+++ cpu_alert.rb
@@ -4,8 +4,8 @@ max = 3
 
 load = `sysctl -n vm.loadavg`
 puts load
-load5 = /^\{ \S+ (\S+)/.match(load)[1].to_f
+load5 = load.split(" ")[2].to_f
 puts load5
 if load5 > max
-  `osascript -e 'display notification "5 minute load is above max" with title "HIGH LOAD"'`
+  `osascript -e 'display notification "5 minute load is above max\n#{load5} > #{max}" with title "High Load"'`
 end
